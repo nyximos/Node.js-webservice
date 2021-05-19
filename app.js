0689c14d248b5327fb06591164ce8fa125f93b61
nyximos <nyximos@gmail.com>
2021-05-19 17:17:05 +0900

    서버 연결 오류 수정

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const pageRouter = require('./routes/page');
 const app = express();
 app.set('port', process.env.PORT || 8001);
 app.set('view engine', 'html');
-nunjucks.configure('views', {
+nunjucks.configure('view', {
   express: app,
   watch: true,
 });
@@ -34,18 +34,18 @@ app.use(session({
 
 app.use('/', pageRouter);
 
-app.use((req, res, next) => {
-  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-  error.status = 404;
-  next(error);
-});
+// app.use((req, res, next) => {
+//   const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+//   error.status = 404;
+//   next(error);
+// });
 
-app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
-  res.status(err.status || 500);
-  res.render('error');
-});
+// app.use((err, req, res, next) => {
+//   res.locals.message = err.message;
+//   res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기중');
--- routes/page.js
+++ routes/page.js
@@ -1,58 +1,58 @@
 const express = require('express');
-
 const router = express.Router();
 
-router.get('/login', (req, res) => {
-    res.render('signin', { title: 로그인});
+router.use((req,res,next) => {
+    // 나중에 템플릿 엔진에서 사용할 변수들을 선언하는 곳
+    next();
+})
+
+router.get('/login', (req, res,next) => {
+    res.render('signin', { title: '로그인'});
 });
 
 router.get('/login/check', (req, res) => {
-    res.render('login', { title: 약관동의});
+    res.render('login', { title: '약관동의'});
 });
 
 router.get('/login/check/signup', (req, res) => {
-    res.render('signup', { title: 회원가입});
+    res.render('signup', { title: '회원가입'});
 });
 
 router.get('/login/find', (req, res) => {
-    res.render('find', { title: 비밀번호재설정});
+    res.render('find', { title: '비밀번호 재설정'});
 });
 
 router.get('/mypage', (req, res) => {
-    res.render('mypage', { title: 마이페이지});
+    res.render('mypage', { title: '마이페이지'});
 });
 
 router.get('/intro', (req, res) => {
-    res.render('introduction', { title: 조원소개});
+    res.render('introduction',{title: '조원소개'})
 });
 
 router.get('/major', (req, res) => {
-    res.render('major', { title: 전공소개});
+    res.render('major', { title: '전공소개'});
 });
 
 router.get('/major/cp', (req, res) => {
-    res.render('major_cp', { title: cp소개});
+    res.render('major_cp', { title: 'cp소개'});
 });
 
 router.get('/major/wd', (req, res) => {
-    res.render('major_wd', { title: wd소개});
+    res.render('major_wd', { title: 'wd소개'});
 });
 
 router.get('/major/gc', (req, res) => {
-    res.render('major_gc', { title: gc소개});
+    res.render('major_gc', { title: 'gc소개'});
 });
 
 router.get('/questions', (req, res) => {
-    res.render('questions', { title: 문의});
+    res.render('questions', { title: '문의'});
 });
 
 
 router.get('/', (req,res,next) => {
-    const twits = [];
-    res.render('index', {
-        title: 'node.js-webservice',
-        twits,
-    });
+    res.render('index', {title: 'node.js-webservice'});
 });
 
 module.exports = router;
