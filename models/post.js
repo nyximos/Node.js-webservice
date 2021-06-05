f9013190312bea2160041512fe547b0493127850
Sangyong-Jeon <tkddyd420@naver.com>
2021-06-05 16:37:26 +0900

    게시글날짜넣기

--- models/post.js
+++ models/post.js
@@ -4,7 +4,6 @@ module.exports = class Post extends Sequelize.Model {
   static init(sequelize) {    // 테이블 생성을 위해 
     return super.init({
       // field설정, id필드는 자동으로 프라이머리키로서 생성
-      
       content: {
         type: Sequelize.STRING(1000),
         allowNull: false,
@@ -13,8 +12,6 @@ module.exports = class Post extends Sequelize.Model {
         type: Sequelize.STRING(200),
         allowNull: true,
       },
-      
-      
     }, {
       sequelize,
       timestamps: true,
