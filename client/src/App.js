fe3f807f7ad4fdcfd030c7b3439da43901f829b4
nyximos <nyximos@gmail.com>
2021-06-06 13:34:34 +0900

    변수에 저장 확인

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,54 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="information">
-        <label>Name:</label>
-        <input type="text" />
-        <label>Student ID:"</label>
-        <input type="number" />
-        <label>Position:</label>
-        <input type="text" />
-        <label>Contact:</label>
-        <input type="text" />
-        <button>Add Member</button>
-      </div>
-    </div>
-  );
+    const [name, setName] = useState("");
+    const [studentId, setStudentId] = useState(0);
+    const [age, setAge] = useState(0);
+    const [position, setPosition] = useState("");
+    const [contact, setContact] = useState("");
+
+    const displayInfo = () => {
+      console.log(name + studentId + age + position + contact)
+    }
+
+    return (
+        <div className="App">
+            <div className="information">
+                <label>Name:</label>
+                <input
+                    type="text"
+                    onChange={(event) => {
+                        setName(event.target.value);
+                    }}/>
+                <label>Student ID:</label>
+                <input
+                    type="number"
+                    onChange={(event) => {
+                        setStudentId(event.target.value);
+                    }}/>
+                <label>Age:"</label>
+                <input
+                    type="number"
+                    onChange={(event) => {
+                        setAge(event.target.value);
+                    }}/>
+                <label>Position:</label>
+                <input
+                    type="text"
+                    onChange={(event) => {
+                        setPosition(event.target.value);
+                    }}/>
+                <label>Contact:</label>
+                <input
+                    type="text"
+                    onChange={(event) => {
+                        setContact(event.target.value);
+                    }}/>
+                <button onClick={displayInfo}>Add Member</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
