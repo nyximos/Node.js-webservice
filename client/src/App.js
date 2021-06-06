25d400f06a2d54d9f48f615b3f63d9e06a88c4b0
nyximos <nyximos@gmail.com>
2021-06-06 13:17:22 +0900

    info

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="information">
+        <label>Name:</label>
+        <input type="text" />
+        <label>Student ID:"</label>
+        <input type="number" />
+        <label>Position:</label>
+        <input type="text" />
+        <label>Contact:</label>
+        <input type="text" />
+      </div>
     </div>
   );
 }
