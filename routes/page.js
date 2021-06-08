e4aca5e3d4a3a2fc44f52d8e3710dd572c470eee
Sangyong-Jeon <tkddyd420@naver.com>
2021-06-08 11:01:34 +0900

    페이지추가
    웹디전공소개페이지
    질의응답페이지
    마이페이지

--- routes/page.js
+++ routes/page.js
@@ -20,6 +20,30 @@ router.get('/join', isNotLoggedIn, (req, res) => {
   res.render('join', { title: '회원가입 - NodeBird' });
 });
 
+router.get('/major', isNotLoggedIn, (req, res) => {
+  res.render('major', { title: '전공소개 - NodeBird' });
+});
+
+router.get('/mypage', isNotLoggedIn, (req, res) => {
+  res.render('mypage', { title: '마이페이지 - NodeBird' });
+});
+
+router.get('/questions', isNotLoggedIn, (req, res) => {
+  res.render('questions', { title: '질의응답 - NodeBird' });
+});
+
+router.get('/questions/view', isNotLoggedIn, (req, res) => {
+  res.render('questions_view', { title: '질의응답_글보기 - NodeBird' });
+});
+
+router.get('/questions/write', isNotLoggedIn, (req, res) => {
+  res.render('questions_write', { title: '질의응답_글쓰기 - NodeBird' });
+});
+
+router.get('/questions/edit', isNotLoggedIn, (req, res) => {
+  res.render('questions_edit', { title: '질의응답_글수정 - NodeBird' });
+});
+
 router.get('/', async (req, res, next) => {
   try {
     const posts = await Post.findAll({
